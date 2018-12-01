0d0714425d457a2288e0d714dafb8adedaf6a89c
jspoet <skihappy17@gmail.com>
2018-11-30 19:38:27 -0800

    uses serverMiddleware

--- packages/ebfnb/src/server/index.js
+++ packages/ebfnb/src/server/index.js
@@ -5,22 +5,17 @@ import usersModule from 'users'
 import tasksModule from 'tasks'
 import serverMiddleware from 'server-middleware'
 const cors = require('cors')
+import {composeModules} from 'tools'
 
-
-const schema=buildASTSchema(typeDefs)
 const mockStore={
   users:[]
 }
-const app = express();
-const graphqlHTTPmiddleware=graphqlHTTP({schema,
-  rootValue:resolvers(store),
+const app = express()
+const m8module=composeModules([userModule,taskModule,{typeDefs,resolvers}])
+app.use(cors())
+app.use('/graphql',serverMiddleware({mockStore,m8module,
   graphiql: true,
   pretty:true
-})
-const drupalMiddleware=drupal({mockStore,
-  userAuthToken:(req)=>req.authorization
-})
-app.use(cors())
-app.use('/graphql',graphqlHTTPmiddleware,drupalMiddleware)
+}))
 app.listen(4000)
 console.log('Running a GraphQL API server at localhost:4000/graphql')
