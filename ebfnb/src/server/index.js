7a76f7709a14641c3f91297a1e3dc8dbfdcea0de
jspoet <skihappy17@gmail.com>
2018-12-01 23:30:15 -0800

    got rid of cors middleware

--- ebfnb/src/server/index.js
+++ ebfnb/src/server/index.js
@@ -1,23 +1,19 @@
 import typeDefs from './typeDefs'
 import resolvers from './resolvers'
 import express from 'express'
-import usersModuleConfig from 'm8-users'
-import tasksModuleConfig from 'm8-tasks'
+import users from 'm8-users/server'
+import tasks from 'm8-tasks/server'
+import volunteerTasks from 'm8-volunteer-tasks/server'
 import serverMiddleware from 'server-middleware'
-const cors = require('cors')
-import {mergeModuleConfigs} from 'tools'
 
 const mockStore={
   users:[]
 }
 const app = express()
-const localMo
-const moduleConfig=mergeModuleConfigs([userModule,taskModule,{typeDefs,resolvers}])
-app.use(cors())
-app.use('/graphql',serverMiddleware({mockStore,
-  moduleConfigs:[usersModuleConfig,tasksModuleConfig],
+app.use('/graphql',serverMiddleware({mockStore,typeDefs,resolvers,
+  moduleConfigs:[users,tasks,volunteerTasks,],
   graphiql: true,
-  pretty:true
+  pretty:true,
 }))
 app.listen(4000)
 console.log('Running a GraphQL API server at localhost:4000/graphql')
