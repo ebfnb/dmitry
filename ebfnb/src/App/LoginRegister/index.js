2c76fbf3202cb61acc50ef02438554dcfbab98b0
jspoet <skihappy17@gmail.com>
2018-12-12 19:34:34 -0800

    finished LoginRegister

--- ebfnb/src/App/LoginRegister/index.js
+++ ebfnb/src/App/LoginRegister/index.js
@@ -1,38 +1,62 @@
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo"
 import React from 'react'
-import LoginForm from './LoginForm'
+import LoginRegisterForm from './LoginRegisterForm'
 import {Redirect} from 'react-router-dom'
+import _ from 'ramda'
+import ApolloConsumer from 'react-apollo'
 
 const login = gql`
-  mutation login($username:String,$password:String){
-      login(username:$username,password:$password){name}
+  mutation login($input:LoginInput){
+      login(input:$input){
+          errors 
+      }
   }
 `
 const register = gql`
-  mutation login($username:String,$password:String){
-      login(username:$username,password:$password){name}
+  mutation register($input:RegisterInput){
+      register(input:$input){
+          errors 
+      }
   }
 `
-export default ({loginRegister}) => {
-    const mutation=(loginRegister==='login')?login:register
+const LoginRegister= ({client,isLogin}) => {
     return (
-        <Mutation mutation={login}>
-            {(login,{error,loading,called})=>{
-                if(called && !error && !loading)return (
-                    <Redirect to='/'/>
-                )
-                const onLogin=({username,password})=>login({
-                    variables:{username,password}}
+        <Mutation mutation={isLogin?login:register}>
+            {(mutate,{loading,called})=>{
+                const {
+                    errors:graphqlErrors,
+                    data:{
+                        errors:loginRegisterErrors,
+                    }
+                }=data
+                const errors=[...graphqlErrors,...loginRegisterErrors]
+                if(called && !errors.length && !loading){
+                    client.resetStore()
+                    return (<Redirect to='/'/>)
+                }
+                const onSubmit=(input)=> mutate({
+                    variables:{input}
+                })
+                const Msg=()=>(
+                    <div>
+                        {_.map((error)=>(
+                            <p>{error}</p>
+                        ))(errors)}
+                    </div>
                 )
                 return (
-                    <LoginForm 
-                        onLogin={onLogin} 
-                        errorMsg={error && "Invalid credentials"}
-                        loading={loading}
-                    />
+                    <div>
+                        <LoginForm onSubmit={onSubmit}/>
+                        <Msg/>
+                    </div>
                 )
             }}
         </Mutation>
     )
-};
+}
+export default (
+    <ApolloConsumer>
+        <LoginRegister/>
+    </ApolloConsumer>
+)
