7e2887abc5f963333e5178a763879c10134d6ea9
jspoet <skihappy17@gmail.com>
2019-01-18 13:56:14 -0800

    created Task.Card

--- ebfnb-client/src/App/containers/Tasks.js
+++ ebfnb-client/src/App/containers/Tasks.js
@@ -9,6 +9,8 @@ import Spinner from "../components/Spinner"
 import LayoutContainer from "../components/LayoutContainer"
 import NavMenuItem from "../components/NavMenuItem"
 import Card from "../components/Card"
+import { dropLast } from "ramda"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 //TaskList component
 const TASK_IDS = gql`
@@ -106,7 +108,7 @@ const TaskList = ({ match }) => {
   return (
     <div css={taskListCss}>
       {TaskIds.map(({ id }) => (
-        <Card id={id} key={id} />
+        <Task.Card id={id} key={id} />
       ))}
     </div>
   )
@@ -118,15 +120,25 @@ const TASK = gql`
   query taskById($id: String) {
     Task(id: $id) {
       id
+      title
       summary
       status {
         label
         reason
       }
-      # commentIds {
-      #   commentId
-      # }
-      # volunteerUserId
+      dateCreated
+      dateOfStatusChange
+      commentIds {
+        commentId
+      }
+      volunteerUserId
+    }
+  }
+`
+const NEW_VOLUNTEER_PROFILE = gql`
+  query userProfileById($id: String) {
+    UserProfile(id: $id) {
+      name
     }
   }
 `
@@ -134,36 +146,67 @@ const cardCss = css`
   border: 1px solid transparent;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   display: flex;
+  flex-direction: column;
   padding: 0.3125rem 0.625rem;
   width: 100%;
   text-decoration: none;
-
   &:hover {
     background-color: #eee;
     border-color: #ddd;
   }
+  div {
+    flex-grow: 1;
+  }
 `
-Task.Card = ({ id, ...props }) => {
-  const {
-    data: { Task }
-  } = useQuery(TASK, { variables: { $id: id } })
 
+Task.Card = ({ id, ...props }) => {
+  // const {
+  //   data: { Task }
+  // } = useQuery(TASK, { variables: { $id: id } })
+  // const {
+  //   data: { UserProfile:newVolunteerProfile}
+  // } = useQuery(TASK, { variables: { $id: Task.volunteerUserId } })
+  const Task = {
+    id: "1",
+    title: "New Volunteer Task",
+    summary:
+      "Out of the blue, in a flash of red, with a lou bang, the call has come, new volunteer, its him, the wait is over",
+    status: {
+      label: "onHold",
+      reason: "waiting for response"
+    },
+    dateCreated: "xmass",
+    dateOfStatusChange: "New year",
+    commentIds: [1, 2, 3, 4],
+    volunteerUserId: "1"
+  }
+  const newVolunteerProfile = { name: "John Doe" }
+  // const shortSummary = dropLast(Task.summary.slice(50).split(" "))
+  //   .push("...")
+  //   .join(" ")
   return (
     <Link css={cardCss} {...props} to="">
-      <span
-        css={css`
-          flex-grow: 1;
-        `}
-      >
-        {Task.status.label}
-      </span>
-      <span
-        css={css`
-          flex-grow: 1;
-        `}
-      >
-        {Task.summary}
-      </span>
+      <div id="header">
+        <h3>{Task.title}</h3>
+        <p>{newVolunteerProfile.name}</p>
+        <p>First contact on: {Task.dateCreated}</p>
+      </div>
+      <div id="status">
+        <p>
+          {Task.status.label} since {Task.dateOfStatusChange}
+        </p>
+        <p>{Task.status.reason}</p>
+      </div>
+      <div id="summary">{Task.summary}</div>
+      <div id="footer">
+        <span className="fa-layers fa-fw" style={{ float: "right" }}>
+          <i className="fas fa-comment" />
+          <span className="fa-layers-counter">{Task.commentIds.length}</span>
+        </span>
+        {/* <span>
+          <FontAwesomeIcon icon="comment" />
+        </span> */}
+      </div>
     </Link>
   )
 }
--- ebfnb-client/src/index.js
+++ ebfnb-client/src/index.js
@@ -2,8 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faSpinner, faCaretDown } from "@fortawesome/free-solid-svg-icons"
+import {
+  faSpinner,
+  faCaretDown,
+  faComment
+} from "@fortawesome/free-solid-svg-icons"
 
 //console.log(spinner)
-library.add({ faSpinner, faCaretDown })
+library.add({ faSpinner, faCaretDown, faComment })
 ReactDOM.render(<App />, document.getElementById("root"))
